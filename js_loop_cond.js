98a68aae3477e7ebeaf2ef98307ba02bea569c05
suryateja2003 <110596986+suryateja2003@users.noreply.github.com>
2022-09-09 11:20:07 +0530

    Add files via upload

---
+++ js_loop_cond.js
@@ -0,0 +1,19 @@
+
+// static way
+
+function even_odd(num)
+{
+	for(i=1;i<=num;i++)
+	{
+	   if(i%2==0)
+ 	   {
+	     console.log(i,"is even");
+	   }
+	   else
+	   {
+	     console.log(i,"is odd");
+	   }
+	}
+}
+
+even_odd(10);
